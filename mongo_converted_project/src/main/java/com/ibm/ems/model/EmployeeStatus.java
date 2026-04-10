5d53e81f3a6297ee5219c491c2524091de48041b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-10 16:23:25 +0530

    mongodb stuff

--- mongo_converted_project/src/main/java/com/ibm/ems/model/EmployeeStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ibm.ems.model;
-
-public enum EmployeeStatus {
-	ACTIVE, ON_LEAVE, TERMINATED
-}
--- mongo_converted_project/src/main/java/com/ibm/ems/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.ibm.ems.repository;
-
-import com.ibm.ems.model.Department;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DepartmentRepository extends MongoRepository<Department, String> {
-}
