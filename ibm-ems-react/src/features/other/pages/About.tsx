c743e740ad7dcbb21a5fa325ee16c7bbfae45927
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-27 15:12:18 +0530

    feature: employees

--- ibm-ems-react/src/features/other/pages/About.tsx
+++ ibm-ems-react/src/features/other/pages/About.tsx
@@ -1,10 +1,40 @@
 // About.tsx
 
+import { useState } from "react";
+
 const About = () => {
+    const [input, setInput] = useState({ username: "", password: "" });
+    const [output, setOutput] = useState({ username: "", password: "" });
+
+    const handleInput = (evt) => {
+        const { name, value } = evt.target;
+        setInput({ ...input, [name]: value });
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setOutput(input);
+        console.log("Form submitted:", input);
+    };
 
     return (
         <>
             <h2>About Component</h2>
+            <p>Forms in ReactJS</p>
+            <hr />
+            <>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={input.username} 
+                    onChange={handleInput} autoFocus />
+                    <br />
+                    <input type="password" value={input.password}
+                        onChange={handleInput} />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </>
+            <p>Input: - {input.username}  - {input.password}</p>
+            <p>Ouput: - {output.username} - {output.password}</p>
         </>
     );
 }
