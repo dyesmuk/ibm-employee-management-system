fbed75b100fa3090ee971c3c379b5f46c2cb7c03
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-29 11:43:41 +0530

    angular initial commit

--- ibm-ems-angular/src/app/components/demo.ts
+++ ibm-ems-angular/src/app/components/demo.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-demo',
-    imports: [RouterOutlet],
     templateUrl: './demo.html'
 })
 export class Demo {
 
 }
+
 
