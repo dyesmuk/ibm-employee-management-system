69e1f5071384db5f8eaa91566e6a541dec23cf0a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-29 12:03:57 +0530

    angular initial commit

--- ibm-ems-angular/src/app/components/demo.ts
+++ ibm-ems-angular/src/app/components/demo.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,6 +9,32 @@ import { RouterOutlet } from '@angular/router';
 })
 export class Demo {
 
+    demoData = { fullName: 'Sonu Pande', salary: 10.25 };
+
 }
+
+
+
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//     selector: 'app-demo',
+//     imports: [],
+//     template: `<div>
+//     <h1>Demo Component</h1>
+//     <p>This is demo component.</p>
+// </div>`,
+// // styles: 'style="color: blue"',
+// // styleUrl: 'styles.css',
+// // styleUrls: ['styles1.css', 'styles2.css']
+// })
+
+// export class Demo {
+
+// }
+
+
+
 
 
