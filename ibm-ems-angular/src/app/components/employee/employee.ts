5e82e78f1a9df3c7171982563da39560d08b5dde
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-29 14:20:16 +0530

    angular components

--- ibm-ems-angular/src/app/components/employee/employee.ts
+++ ibm-ems-angular/src/app/components/employee/employee.ts
@@ -1,11 +1,24 @@
+import { CurrencyPipe, UpperCasePipe, } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [UpperCasePipe, CurrencyPipe, FormsModule],
   templateUrl: './employee.html',
   styleUrl: './employee.css',
 })
 export class Employee {
 
+  employeeData = { firstName: 'Sonu', lastName: 'Joshi', salary: 12.75 }
+
+  demoClick = () => {
+    console.log("button clicked!");
+  };
+
+  isDisabled = false;
+
+  username = "";
+
 }
+
