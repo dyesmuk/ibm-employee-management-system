e75b7eb0603223bcf7b79e75cc2792a1309864bb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-30 12:05:33 +0530

    parent clild communication

--- ibm-ems-angular/src/app/app.routes.ts
+++ ibm-ems-angular/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Parent } from './features/parent/parent';
+import { Department } from './features/department/department';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: 'parent', component: Parent },
+    { path: 'department', component: Department },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     // { path: '**', component: Page404 }
 ];
--- ibm-ems-angular/src/app/app.ts
+++ ibm-ems-angular/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Demo } from './components/demo/demo';
-import { Employee } from './components/employee/employee';
-import { Parent } from './features/parent/parent';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Demo, Employee, Parent],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
