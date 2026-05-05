4743eac2fea110a9aed5fcabc89f04934074116e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-05-05 15:21:24 +0530

    forms in angular

--- ibm-ems-angular/src/app/features/department/department-create/department-create.ts
+++ ibm-ems-angular/src/app/features/department/department-create/department-create.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Department } from '../../../core/models/department.model';
 import { DepartmentService } from '../../../core/services/department/department.service';
+
+// see this 
+// https://angular.dev/guide/forms
 
 @Component({
   selector: 'app-department-create',
@@ -22,24 +25,64 @@ export class DepartmentCreate {
 
   constructor(private departmentService: DepartmentService) { }
 
-  createDepartment() {
-
+  createDepartment(form: NgForm) {
     this.departmentService.addDepartment(this.department).subscribe({
-      next: (res) => {
-        console.log(res);
+      next: () => {
         this.message = 'Department created successfully';
         this.error = '';
-
-        this.department = {
-          name: '',
-          description: ''
-        };
+        form.resetForm();
       },
-      error: (err) => {
+      error: () => {
         this.error = 'Failed to create department';
+        this.message = '';
       }
     });
   }
 }
+
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { Department } from '../../../core/models/department.model';
+// import { DepartmentService } from '../../../core/services/department/department.service';
+
+// @Component({
+//   selector: 'app-department-create',
+//   standalone: true,
+//   imports: [FormsModule, CommonModule],
+//   templateUrl: './department-create.html',
+// })
+// export class DepartmentCreate {
+
+//   department: Department = {
+//     name: '',
+//     description: ''
+//   };
+
+//   message = '';
+//   error = '';
+
+//   constructor(private departmentService: DepartmentService) { }
+
+//   createDepartment() {
+
+//     this.departmentService.addDepartment(this.department).subscribe({
+//       next: (res) => {
+//         console.log(res);
+//         this.message = 'Department created successfully';
+//         this.error = '';
+
+//         this.department = {
+//           name: '',
+//           description: ''
+//         };
+//       },
+//       error: (err) => {
+//         this.error = 'Failed to create department';
+//       }
+//     });
+//   }
+// }
+
 
 
