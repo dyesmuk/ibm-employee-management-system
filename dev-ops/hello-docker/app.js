8f3c7131d7cbeddc1df64a064c43d69047d0bf9b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-05-07 14:36:51 +0530

    docker final docs

--- dev-ops/hello-docker/app.js
+++ dev-ops/hello-docker/app.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+mongoose.connect('mongodb://mongo:27017/hellodb')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB error:', err));
+
 app.get('/', (req, res) => {
-  res.send('Hello from Express inside Docker!');
+  const status = mongoose.connection.readyState === 1 ? 'Connected ✅' : 'Disconnected ❌';
+  res.send(`Hello from Express! MongoDB: ${status}`);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
 
-// const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//   res.end('Hello from inside Docker!');
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from Express inside Docker!!!!');
 // });
 
-// server.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000'));
+
+// // const http = require('http');
+
+// // const server = http.createServer((req, res) => {
+// //   res.end('Hello from inside Docker!');
+// // });
+
+// // server.listen(3000, () => console.log('Server running on port 3000'));
